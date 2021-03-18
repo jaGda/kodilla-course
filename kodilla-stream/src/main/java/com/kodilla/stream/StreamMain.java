bb4fb70dbf71da58007c4063c171d7ff726fe5e8
jagda <danielzychniewicz@wp.pl>
2021-03-18 19:45:54 +0100

    7.2 Stream - what is it?

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,10 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-
-        poemBeautifier.beautify("Java", text -> text.concat(" world"));
-        poemBeautifier.beautify("Java", String::toUpperCase);
-        poemBeautifier.beautify("Java", "What is "::concat);
-        poemBeautifier.beautify("Java", "Why "::concat);
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
