86eaf03c825bb831b7af4d61141438a9c8868a3b
jagda <danielzychniewicz@wp.pl>
2021-03-21 12:24:24 +0100

    Modified StreamMain.class - used ChronoUnit.class instead of Period.class in main method

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -4,19 +4,15 @@ import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
 import java.time.LocalDate;
-import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
 
-        LocalDate date = LocalDate.of(1985, 12, 9);
-
-        System.out.println(Period.between(date, LocalDate.now()).getYears());
-
         Forum.getUsersList().stream()
                 .filter(forumUser -> forumUser.getSex() == 'M')
-                .filter(forumUser -> Period.between(forumUser.getDateOfBirth(), LocalDate.now()).getYears() > 100)
+                .filter(forumUser -> ChronoUnit.YEARS.between(forumUser.getDateOfBirth(), LocalDate.now()) >= 100)
                 .filter(forumUser -> forumUser.getPostedPosts() >= 1)
                 .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser))
                 .entrySet().stream()
