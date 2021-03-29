413b3eb3746d669d6f56379bbc5d282b16701a93
jagda <danielzychniewicz@wp.pl>
2021-03-29 18:26:15 +0200

    8.4 Error handling

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/nullpointer/OptionalExample.java
@@ -0,0 +1,14 @@
+package com.kodilla.exception.nullpointer;
+
+import java.util.Optional;
+
+public class OptionalExample {
+
+    public static void main(String[] args) {
+        User user = new User("user1");
+        Optional<User> optionalUser = Optional.ofNullable(user);
+        String userName = optionalUser.orElse(new User("")).getName();
+        System.out.println(userName);
+        optionalUser.ifPresent(u -> System.out.println(u.getName()));
+    }
+}
