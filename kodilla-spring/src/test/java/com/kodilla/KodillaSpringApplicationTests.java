8bee97e8a2254c49f21faf174dd6dea3d58624ce
jagda <danielzychniewicz@wp.pl>
2021-05-09 10:27:56 +0200

    14.3 Configuration by Class

--- kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
@@ -1,15 +1,30 @@
 package com.kodilla;
 
+import com.kodilla.spring.library.LibraryConfig;
 import com.kodilla.spring.shape.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class KodillaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        //When & Then
+        System.out.println("===== Beans list: ===>>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<<=== Beans list: =====");
+    }
 
     @Test
     void testShapeLoadedIntoContainer() {
@@ -65,9 +80,5 @@ class KodillaSpringApplicationTests {
 
         //Then
         assertEquals("This is a Triangle", name);
-    }
-
-    @Test
-    void contextLoads() {
     }
 }
