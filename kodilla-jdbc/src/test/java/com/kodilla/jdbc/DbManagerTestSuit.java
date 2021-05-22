da2277a2e45fceb948b6d0090b11aed31413ec39
jagda <danielzychniewicz@wp.pl>
2021-05-22 21:04:06 +0200

    16.6 Task - JDBC

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuit.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuit.java
@@ -12,6 +12,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class DbManagerTestSuit {
 
     @Test
+    void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        //When
+        String sqlQuery = "SELECT kodilla_course.USERS.FIRSTNAME, kodilla_course.USERS.LASTNAME, COUNT(*) as POSTS_NUMBER " +
+                "from kodilla_course.USERS " +
+                "         join kodilla_course.posts on kodilla_course.USERS.ID = kodilla_course.POSTS.USER_ID " +
+                "GROUP BY kodilla_course.USERS.FIRSTNAME, kodilla_course.USERS.LASTNAME " +
+                "having POSTS_NUMBER >= 2;";
+        Statement statement = DB_MANAGER.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("FIRSTNAME") + "|" +
+                    resultSet.getString("LASTNAME") + "|" +
+                    resultSet.getInt("POSTS_NUMBER"));
+            counter++;
+        }
+        assertEquals(5, counter);
+    }
+
+    @Test
     void testConnection() throws SQLException {
         //Given
         //When
